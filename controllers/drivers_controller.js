5264e085e6e535d0ff2fbd6f9ce0bc38728987dd
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-18 22:45:59 -0400

    Testing geoqueries

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -9,9 +9,11 @@ module.exports = {
     const { lng, lat } = req.query;
 
     Driver.geoNear(
-      { type: 'Point', coordinates: [lng, lat] },
+      { type: 'Point', coordinates: [parseFloat(lng), parseFloat(lat)] },
       { spherical: true, maxDistance: 200000 }
     )
+      .then(drivers => res.send(drivers))
+      .catch(next);
   },
 
   create(req, res, next) {
